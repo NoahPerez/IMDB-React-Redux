769ce0a07634d4f2858e78d6c357e22546cf9648
Noah <noahperezlachaud@gmail.com>
2020-03-25 21:30:24 +0000

    Updates

---
+++ precache-manifest.f6a77b8b57cb924211b9e44d50fcd1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a044fd2ab1e295d675255c3e4f4b6901",
+    "url": "/IMDB-React-Redux/index.html"
+  },
+  {
+    "revision": "6af615bce85da38f9d32",
+    "url": "/IMDB-React-Redux/static/css/main.52226f6f.chunk.css"
+  },
+  {
+    "revision": "a60b988e8d15667c449c",
+    "url": "/IMDB-React-Redux/static/js/2.0cba65d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IMDB-React-Redux/static/js/2.0cba65d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af615bce85da38f9d32",
+    "url": "/IMDB-React-Redux/static/js/main.8444b128.chunk.js"
+  },
+  {
+    "revision": "a235a754922fddb9e3a9",
+    "url": "/IMDB-React-Redux/static/js/runtime-main.e7739be8.js"
+  },
+  {
+    "revision": "e26d873ee68fbcd9dc1c40d8b0c6b3b4",
+    "url": "/IMDB-React-Redux/static/media/spinner.e26d873e.gif"
+  }
+]);
